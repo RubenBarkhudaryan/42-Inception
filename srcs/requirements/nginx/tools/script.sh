8cc03dc44a5cf723795596957619646ae8904e6a
rbarkhud <rubenbarkhudaryan2005@gmail.com>
2026-06-02 23:10:03 +0400

    nginx server rises correctly with simple configs

--- srcs/requirements/nginx/tools/script.sh
+++ srcs/requirements/nginx/tools/script.sh
@@ -2,11 +2,14 @@
 
 set -e
 
+CERT_DIR="/etc/nginx/ssl"
+DOMAIN_NAME="rbarkhud.42.fr"
+
 mkdir -p /etc/nginx/ssl
 
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
-			-keyout /etc/nginx/ssl/nginx.key\
-			-out /etc/nginx/ssl/nginx.crt\
-			-subj '/CN=${DOMAIN_NAME:-localhost}'
+			-keyout "$CERT_DIR/nginx.key" \
+			-out "$CERT_DIR/nginx.crt" \
+			-subj "/C=AM/ST=Armenia/L=Yerevan/O=42/OU=Inception/CN=${DOMAIN_NAME}"
 
 exec "$@"
