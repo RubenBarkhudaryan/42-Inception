9ab2080f8a86ea7e2a79242115cedcb9a1355e5a
RubenBarkhudaryan <rubenbarkhudaryan2005@gmail.com>
2026-06-10 21:09:58 +0400

    mariadb

---
+++ srcs/requirements/mariadb/tools/script.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+service mariadb start
+
+sleep 3
+
+MYSQL_DB_USER=${MYSQL_DB_USER:-wordpress}
+MYSQL_DB_NAME=${MYSQL_DB_NAME:-wordpress}
+MYSQL_PASSWORD=$(cat ${MYSQL_PASSWORD_FILE})
+MYSQL_ROOT_PASSWORD=$(cat ${MYSQL_ROOT_PASSWORD_FILE})
+
+echo -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DB_NAME};
+CREATE USER '${MYSQL_DB_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+GRANT ALL PRIVILEGES ON ${MYSQL_DB_NAME}.* TO '${MYSQL_DB_USER}'@'%';
+FLUSH PRIVILEGES;" > db.sql
+
+mysql -u root -p${MYSQL_ROOT_PASSWORD} < db.sql
+
+rm db.sql
+
+service mariadb stop
+
+exec "$@"
